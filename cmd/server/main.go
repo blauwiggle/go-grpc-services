c7a8b3ced34fe807b6f0e00ca5f47dc35a348514
Michael Vanhee <michael.vanhee@icloud.com>
2022-05-27 12:53:30 +0200

    Add database connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/blauwiggle/go-grpc-services/internal/db"
+	"github.com/blauwiggle/go-grpc-services/internal/rocket"
+)
 
 func Run() error {
 	// responsible for init and starting our gRPC server
+
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+
+	_ = rocket.New(rocketStore)
+
 	return nil
 }
 
